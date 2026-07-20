8d384f844d89febf4bbf69236233fd56caf7c0d0
OscarLi <lcw.1994@qq.com>
2026-07-20 15:25:57 +0800

    完善深度研究流程

--- examples/mini_deep_research.py
+++ examples/mini_deep_research.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from pydantic import BaseModel
+from langchain_tavily import TavilySearch
+from langgraph.prebuilt import create_react_agent
 
 load_dotenv(override=True)
 
@@ -45,3 +47,87 @@ class WebSearchPlan(BaseModel):
     searches: list[WebSearchItem]
     "A list of web searches to perform to best answer the query"
     "为了尽可能全面回答该问题而需要执行的网页搜索列表"
+
+planner_chain = planner_prompt | model.with_structured_output(WebSearchPlan)
+
+planner_result = planner_chain.invoke({'query': '请问你对AI+教育有何看法'})
+
+SEARCH_INSTRUCTIONS = (
+    "You are a research assistant. Given a search term, you search the web for that term and "
+    "produce a concise summary of the results. The summary must 2-3 paragraphs and less than 300"
+    "words. Capture the main points. Write succinctly, no need to have complete sentences or good"
+    "grammar. This will be consumed by someone synthesizing a report, so its vital you capture the "
+    "essence and ignore any fluff. Do not include any additional commentary other than the summary itself."
+)
+
+
+search_tool = TavilySearch(max_results=5, topic="general")
+
+search_agent = create_react_agent(
+    model,
+    prompt=SEARCH_INSTRUCTIONS,
+    tools=[search_tool]
+)
+
+WRITER_PROMPT = (
+    "You are a senior researcher tasked with writing a cohesive report for a research query."
+    "You will be provided with the original query, and some initial research done by a research assistant. \n"
+    "You should first come up with an outline for the report that describes the structure and flow of the report. Then, "
+    "generate the report and return that as your final output. \n The final output should be in markdown format, and it should"
+    "be lengthy and detailed. Aim for 10-20 pages of content, at least 1500 words. 最终生成的报告采用中文输出."
+)
+
+writer_prompt = ChatPromptTemplate.from_messages([
+    ('system', WRITER_PROMPT),
+    ('human', '{query}')
+])
+
+# 生成关键词规划
+def plan_searches(query: str) -> WebSearchPlan:
+    result = planner_chain.invoke({'query': query})
+    return result
+
+# 根据关键词进行搜索
+def search(item:WebSearchItem) -> str | None:
+    try:
+        final_query = f"Search Item: {item.query}\nReason for searching: {item.reason}"
+        result = search_agent.invoke({"messages":[
+            {
+                "role": "user",
+                "content": final_query
+            }
+        ]})
+        return str(result['messages'][-1].content)
+    except Exception:
+        return None
+
+# 根据关键词列表逐个搜索得到搜索结果列表
+def perform_searches(search_plan: WebSearchPlan):
+    results = []
+    for item in search_plan.searches:
+        result = search(item)
+        if result is not None:
+            results.append(result)
+    return results
+
+# 根据搜索的结果列表和用户提问生成报告
+def write_report(query: str, search_results) -> ReportData:
+    summary=''
+    for search_result in search_results:
+        summary += search_result
+    final_query = f'Original query: {query}\n Summarized search results: {summary}'
+    result = writer_chain.invoke({
+        'query': final_query
+    })
+    return result
+
+# 串联以上流程函数
+def deepresearch(query: str) -> ReportData:
+    '''
+    输入一个研究主题，自动完成搜索规划、搜索和写报告
+    返回最终的ReportData对象，就是一个markdown的格式完整的研究报告文档
+    '''
+    search_plan = plan_searches(query)
+    search_results = perform_searches(search_plan)
+    report = write_report(query, search_results)
+    print(report.markdown_report)
